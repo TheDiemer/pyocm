6e0675d251b5299917ba10f76fd2744b999d75ef
Eric Jones <erjones@navi.usersys.redhat.com>
2019-10-03 11:18:14 -0400

    Initial commit

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyhydra",
+    name="pyocm",
     version="0.0.1",
-    author="Eric Rich",
-    author_email="erich@redhat.com",
-    description="Python API for interacting with Hydra",
+    author="Eric Jones",
+    author_email="erjones@redhat.com",
+    description="Python API for interacting with OCM",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.cee.redhat.com/erich/pyhydra",
+    url="https://gitlab.cee.redhat.com/erjones/pyocm",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
