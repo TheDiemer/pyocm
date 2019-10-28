4df6fb2da8773e5956dfdb2a8f7232b58b33762b
Eric Jones <erjones@navi.usersys.redhat.com>
2019-10-28 17:46:43 -0400

    keeping the tests up to date as well

--- tests/test_ocm.py
+++ tests/test_ocm.py
@@ -23,33 +23,38 @@ class TestOCM(unittest.TestCase):
         self.ocm = pyocm.ocm_api(token=config.get('ocm', 'token'))
 
 
-    @unittest.skipUnless(DEBUG, "skipping manuall debugging test")
+    @unittest.skipUnless(DEBUG, "skipping manual debugging test")
     def test_org_by_account(self):
         r = self.ocm.get_org_by_account(ACCOUNT_NUMBER)
         print(json.dumps(r,sort_keys=True, indent=INDENT))
         self.assertTrue(True)
    
 
-    @unittest.skipUnless(DEBUG, "skipping manuall debugging test")
+    @unittest.skipUnless(DEBUG, "skipping manual debugging test")
     def test_account_by_org(self):
         r = self.ocm.get_account_by_org(ORG_ID)
         print(json.dumps(r,sort_keys=True, indent=INDENT))
         self.assertTrue(True)
    
 
-    @unittest.skipUnless(DEBUG, "skipping manuall debugging test")
+    @unittest.skipUnless(DEBUG, "skipping manual debugging test")
     def test_subs_by_creator(self):   # Subs is clusters
         r = self.ocm.get_subs_by_creator(CREATOR_ID)
         print(json.dumps(r,sort_keys=True, indent=INDENT))
         self.assertTrue(True)
     
 
-    @unittest.skipUnless(DEBUG, "skipping manuall debugging test")
+    @unittest.skipUnless(DEBUG, "skipping manual debugging test")
     def test_subs_by_cluster(self):   # Subs is clusters
         r = self.ocm.get_subs_by_cluster_id(CLUSTER_ID)
         print(json.dumps(r,sort_keys=True, indent=INDENT))
         self.assertTrue(True)
    
+    @unittest.skipUnless(DEBUG, "skipping manual debugging test")
+    def test_user_account_info(self): 
+        r = self.ocm.get_current_account()
+        print(json.dumps(r,sort_keys=True, indent=INDENT))
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
